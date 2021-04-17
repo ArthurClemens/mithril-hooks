f8ed92e58ace5cec07d9b50622e9f201eebea703
Arthur Clemens <arthurclemens@gmail.com>
2021-04-17 18:39:35 +0200

    Minor

--- packages/mithril-hooks/src/types/ReactTypes.ts
+++ packages/mithril-hooks/src/types/ReactTypes.ts
@@ -1,4 +1,6 @@
 declare const UNDEFINED_VOID_ONLY: unique symbol;
-export type Destructor = () => void | { [UNDEFINED_VOID_ONLY]: never };
-export type EffectCallback = () => void | Destructor;
+// Destructors are only allowed to return void.
+type Destructor = () => void | { [UNDEFINED_VOID_ONLY]: never };
+
 export type DependencyList = ReadonlyArray<unknown>;
+export type EffectCallback = () => void | Destructor;
